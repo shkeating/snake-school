1f0bf2ffdcb39191e4cd0a0b6472a5e400eea967
Shauna Keating <59394696+shkeating@users.noreply.github.com>
2024-06-18 15:00:14 +0000

    grammar fixes

--- units.py
+++ units.py
@@ -1,6 +1,6 @@
 # convert tablespoons to teaspoons
 
-tablespoons = int (input('Enter number of tablespoons: ')) #will take a whole number input from user
+tablespoons = int (input('Enter your number of tablespoon(s): ')) #will take a whole number input from user
 teaspoons = tablespoons * 3
 
-print(str (tablespoons) + ' tablespoons is equal to ' + str (teaspoons) + ' teaspoons') #outputs number of teaspoons to the user
+print(str (tablespoons) + ' tablespoon(s) is equal to ' + str (teaspoons) + ' teaspoons.') #outputs number of teaspoons to the user
