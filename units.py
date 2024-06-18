61af06cde63fac3b5536d78c5e15f58475ab18cf
Shauna Keating <59394696+shkeating@users.noreply.github.com>
2024-06-18 14:58:45 +0000

    adds string interpolation to provide the user with an easier to understand output, fixes operator to multiply by 3

--- units.py
+++ units.py
@@ -1,6 +1,6 @@
 # convert tablespoons to teaspoons
 
 tablespoons = int (input('Enter number of tablespoons: ')) #will take a whole number input from user
-teaspoons = tablespoons / 3
+teaspoons = tablespoons * 3
 
-print(teaspoons) #outputs number of teaspoons to the user
+print(str (tablespoons) + ' tablespoons is equal to ' + str (teaspoons) + ' teaspoons') #outputs number of teaspoons to the user
