01974253c545d121d9eb765194e3fa5f248d2c72
Shauna Keating <shauna@truss.works>
2024-06-18 10:47:38 -0400

    adds pront command to output number of teaspoons

--- units.py
+++ units.py
@@ -2,3 +2,5 @@
 
 tablespoons = int (input('Enter number of tablespoons: ')) #will take a whole number input from user
 teaspoons = tablespoons / 3
+
+print(teaspoons) #outputs number of teaspoons to the user
