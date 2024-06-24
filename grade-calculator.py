c346691dd15d522ba76654b184b21b5a87f599c1
Shauna Keating <59394696+shkeating@users.noreply.github.com>
2024-06-24 02:25:52 +0000

    adds in user prompts to collect variable values for grade inputs

--- grade-calculator.py
+++ grade-calculator.py
@@ -5,3 +5,13 @@
 # each type of assignment
 
 # variable initalization
+
+weight_discussion = 0.36
+weight_assignment = 0.36
+weight_exercise = 0.14
+weight_quiz = 0.14
+
+earned_discussion = input('please enter a numerical percentage grade form 0-100 of discussion posts')
+earned_assignment = input('please enter a numerical percentage grade form 0-100 of weekly assignments')
+earned_exercise = input('please enter a numerical percentage grade form 0-100 of w3 exercises')
+earned_quiz = input('please enter a numerical percentage grade form 0-100 of w3 quizzes')
