92285db93f06332c83e4c5b665fd8327e1d802e0
Shauna Keating <59394696+shkeating@users.noreply.github.com>
2024-06-24 02:38:37 +0000

    adds in calculations and final user output with calculated grade

--- grade-calculator.py
+++ grade-calculator.py
@@ -6,12 +6,27 @@
 
 # variable initalization
 
+# assignment type weights
 weight_discussion = 0.36
 weight_assignment = 0.36
 weight_exercise = 0.14
 weight_quiz = 0.14
 
-earned_discussion = input('please enter a numerical percentage grade form 0-100 of discussion posts')
-earned_assignment = input('please enter a numerical percentage grade form 0-100 of weekly assignments')
-earned_exercise = input('please enter a numerical percentage grade form 0-100 of w3 exercises')
-earned_quiz = input('please enter a numerical percentage grade form 0-100 of w3 quizzes')
+# earned grades inputs
+earned_discussion = input('please enter a numerical percentage grade from 0-100 of discussion posts ')
+earned_assignment = input('please enter a numerical percentage grade from 0-100 of weekly assignments ')
+earned_exercise = input('please enter a numerical percentage grade from 0-100 of w3 exercises ')
+earned_quiz = input('please enter a numerical percentage grade from 0-100 of w3 quizzes ')
+
+# multiply each percentage by its cooresponding category weight
+
+discussion = weight_discussion * float(earned_discussion)
+assignment = weight_assignment * float(earned_assignment)
+exercise = weight_exercise * float(earned_exercise)
+quiz = weight_quiz * float(earned_quiz)
+
+# sum the categories to get output of numerical grade earned
+grade = discussion + assignment + exercise + quiz
+
+#print final grade
+print('your final grade calculates to' + grade + '.')
