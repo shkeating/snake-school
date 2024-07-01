c447c9679b8f2676b8cacd53e1881a9cccba6268
Shauna Keating <59394696+shkeating@users.noreply.github.com>
2024-07-01 02:02:01 +0000

    refactors for correct try except usage

--- longest_word.py
+++ longest_word.py
@@ -1,11 +1,12 @@
 # accepts a phrase from user letter-only input with word limit and determines which word in the phrase is the longest by containing the most characters
+phrase = ""
 
-phrase = input ("Enter a sentence, phrase, series of words not exceeding 14 words in length")
-
-try:
-    word_list = phrase.split()
-except:
-    print('Please enter a phrase using alphbetical characters')
-else: 
-    if word_list.length > 14 
-        print("Please try again with a shorter phrase, the limit is 14 words.")
+#while loop ensures the program keeps asking for input until the user provides a valid string.
+while True:
+    #accepts user input
+    user_phrase = input("Please enter a string: ")
+    
+    try:
+        #check if input is a string
+        if not type(user_phrase) is str:
+            raise TypeError("Only strings are allowed")
