bce57b22c654a17c4431f1e557e18054aca04f1c
Shauna Keating <59394696+shkeating@users.noreply.github.com>
2024-07-01 02:41:00 +0000

    edits comments to be more useful

--- longest_word.py
+++ longest_word.py
@@ -12,17 +12,17 @@ while True:
             print("The input string is too long (15 words or more).") #ensures user sees what they did wrong
             raise ValueError() # makes the program stop and re prompt for input 
     
-    except: # if it for some reason fails prompt user to try again
+    except: # prompt user to try again
         print("invalid input. Please try again.")
 
-    else: # Exit the while loop if the input is valid
+    else: # Exit the while loop if the input is valid and continue on
         print("Thanks. Let's see which of those words is the longest in character length!")
         break  
 
 #turn user phrase into a list stored in word list variable
-words = user_phrase.split()
-longest_word = ""
-max_length = 0
+words = user_phrase.split() #creates word array from string
+longest_word = "" #stores variable for longest word 
+max_length = 0 #starging word count for longest word computation
 
 #set up list to store letter counts in
 letter_counts = []
@@ -36,8 +36,8 @@ for word in words:
         longest_word = word
         max_length = len(word)
 
-# pairs words with their counts and prints out all the users words with how many letters are in them. f string used to make string interpolation less annoying (or, i guess more like javascript lol)
-for word, count in zip(words, letter_counts):
+# compares word counts and prints out all the users words with how many letters are in them. 
+for word, count in zip(words, letter_counts): #zip makes a nested list of the words with their counts paired together
     print(f"The word '{word}' has {count} letters.")
-
+	#f string used to make string interpolation less annoying (or, i guess more like javascript lol)
 print(f"The longest word is '{longest_word}' with {max_length} letters.")
