ffdd5d5e9ea7a5cfafd28199197e03c0d7e95ef8
Shauna Keating <59394696+shkeating@users.noreply.github.com>
2024-07-08 02:16:03 +0000

    got bubble sort to work and refactored program a bit to account for the fact dictionaries are unsorted

--- alphabetical.py
+++ alphabetical.py
@@ -27,6 +27,18 @@ print("...")
 
 words = dict()
 
+"""
+the dictionary data formal will look like this when populated:
+
+words = {
+    "cat": ['c', 'a', 't'],
+    "egg": ['e','g','g'],
+    "rat": ['r','a','t']
+}
+
+"""
+
+
 #
 ##
 ### collect words and populate dictionary
@@ -38,22 +50,47 @@ while True:
     word = input("Enter any word, or enter 0 to finish: ").strip()
     if word == "0":
         break
-    # store the letters of the word in the value as a list
+    # store the letters of the word in the value as a list, see comment in data structures for example of intended data structure
     words[word] = list(word)
+
+print("...")
+print("Thanks for all those words! Time to get sorting")
+print("...")
+print(f"{words.values}") # showing the user all the letters back for dramatic effect.. i didn't end up needing those lists of letters.. might as well use them for something. dictionaries are undorderd so I actually cant refer to them by index like how i thought i could going into this!
+print("...")
+print("Done!")
+print("Here is your sorted list of words:")
 
 #
 ##
 ### sort the words -- we could use sorted method... 
-##  but that would have eliminated the point of using a dictionary
-#   and would not allow me to practice dictionaries and lists
+##  
+#  
 
-# taking a bubble sort approach
+#   taking a bubble sort approach. we absolutely could use sorted method
+#   but that would have not let me practice dictionaries and lists as much
+#   which would have eliminated the point of the assignment
 
+# Extract the keys (words) from the dictionary, bubble sort is easier to do with a list
+words_list = list(words.keys())
 
+l = len(words_list) #gets the length of our list
 
+for i in range(l): #outer loop that iterates through each word in our list of words
+    for j in range(0, l-i-1): #inner loop where we compare and sort
+         if words_list[j] > words_list[j+1]: #checking the word against the next word in the list
+             #if it starts with a letter further up in the order they get switched
+              words_list[j], words_list[j+1] = words_list[j+1], words_list[j]
 
-### TESTING
+# Since we can't change the order of our original dictionary we will make a new one that uses the new order of our words list and the data from our original list
+sorted_words = {word: words[word] for word in words_list}
 
-# Print the resulting dictionary
-# print("The dictionary of words and their first letters is:")
-# print(words)
+#
+##
+### give the user back their sorted list
+##  
+#  
+
+print(sorted_words.keys())
+print("...")
+print(f"Your word highest in alphabetical order is {words_list[0]}")
