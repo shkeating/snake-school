aedf570d360faa0f97abfeb618bc346c6e18d5ff
Shauna Keating <59394696+shkeating@users.noreply.github.com>
2024-07-15 02:25:04 +0000

    adds if elese to while loop collecting input

--- longest_word_functions.py
+++ longest_word_functions.py
@@ -1,2 +1,14 @@
 # accepts a phrase from user letter-only input with word limit and determines which word in the phrase is the longest by containing the most characters
 
+## collect input from user to get our words
+
+def get_valid_input(prompt, min_words=1, max_words=14):
+    while True:
+        user_input = input(prompt)
+        words = user_input.split()
+        if min_words <= len(words) <= max_words:
+            print("Thanks. Let's see which of those words is the longest in character length!")
+            return words
+        else:
+            print(f"The input string is not the right length (at least {min_words} word, and no longer than {max_words} words). Please try again.")
+    
